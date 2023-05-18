1098f20d42443e1bbcfc047c92728cfda9529d16
Hypersonic-cpu <133350981+Hypersonic-cpu@users.noreply.github.com>
2023-05-18 16:05:04 +0800

    Update Equa2.fs

--- Script.fsx
+++ Script.fsx
@@ -1,9 +1,6 @@
-﻿// Learn more about F# at https://fsharp.org
-// See the 'F# Tutorial' project for more help.
+﻿#load "../ListVec/LVec.fs"
+open LVec
 
 #load "Equa2.fs"
 #load "Equa1.fs"
 open Equation
-
-// Define your library scripting code here
-
